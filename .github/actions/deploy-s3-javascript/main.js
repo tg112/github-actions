ab0b94d15c12d66f2fe06dd41b3604764d9dd7e6
tg112 <info.tgw112@gmail.com>
2024-01-08 10:25:40 +0900

    chore: remove async

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -2,7 +2,7 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const exec = require('@actions/exec')
 
-async function run() {
+function run() {
   core.notice('Hello from my custom js action');
 }
 
